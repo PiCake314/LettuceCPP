f3d0967d0a58ae6d81b2ebf757cc7bc4ce4beb3f
Ali Almutawa <alal5051@colorado.edu>
2023-04-03 05:23:02 -0600

    initial branch commit

--- Expr.hpp
+++ Expr.hpp
@@ -3,56 +3,56 @@
 #include <ostream>
 #include "../Bector/Bector.hpp"
 
-struct Expr{ virtual ~Expr() = default; };
+// struct Expr{ virtual ~Expr() = default; };
 
 
-struct Num : Expr{
-    double value;
-    Num(double v) : value(v) {};
-};
+// struct Num : Expr{
+//     double value;
+//     Num(double v) : value(v) {};
+// };
 
-struct Var : Expr{
-    std::string name;
-    Var(std::string n) : name(n) {};
-};
+// struct Var : Expr{
+//     std::string name;
+//     Var(std::string n) : name(n) {};
+// };
 
-struct Plus : Expr{
-    Expr *lhs;
-    Expr *rhs;
-    Plus(Expr *l, Expr *r) : lhs(l), rhs(r) {};
-};
+// struct Plus : Expr{
+//     Expr lhs;
+//     Expr rhs;
+//     Plus(Expr l, Expr r) : lhs(l), rhs(r) {};
+// };
 
-struct Mult : Expr{
-    Expr *lhs;
-    Expr *rhs;
-    Mult(Expr *l, Expr *r) : lhs(l), rhs(r) {};
-};
+// struct Mult : Expr{
+//     Expr lhs;
+//     Expr rhs;
+//     Mult(Expr l, Expr r) : lhs(l), rhs(r) {};
+// };
 
-struct Div : Expr{
-    Expr *lhs;
-    Expr *rhs;
-    Div(Expr *l, Expr *r) : lhs(l), rhs(r) {};
-};
+// struct Div : Expr{
+//     Expr lhs;
+//     Expr rhs;
+//     Div(Expr l, Expr r) : lhs(l), rhs(r) {};
+// };
 
-struct Minus : Expr{
-    Expr *lhs;
-    Expr *rhs;
-    Minus(Expr *l, Expr *r) : lhs(l), rhs(r) {};
-};
+// struct Minus : Expr{
+//     Expr lhs;
+//     Expr rhs;
+//     Minus(Expr l, Expr r) : lhs(l), rhs(r) {};
+// };
 
-struct Let : Expr{
-    std::string name;
-    Expr *value;
-    Expr *body;
-    Let(std::string n, Expr *v, Expr *b) : name(n), value(v), body(b) {};
-};
+// struct Let : Expr{
+//     std::string name;
+//     Expr value;
+//     Expr body;
+//     Let(std::string n, Expr v, Expr b) : name(n), value(v), body(b) {};
+// };
 
-struct MultiLet : Expr{
-    Bector<std::string> names;
-    Bector<Expr*> values;
-    Expr *body;
-    MultiLet(Bector<std::string> n, Bector<Expr*> v, Expr *b) : names(n), values(v), body(b) {};
-};
+// struct MultiLet : Expr{
+//     Bector<std::string> names;
+//     Bector<Expr> values;
+//     Expr body;
+//     MultiLet(Bector<std::string> n, Bector<Expr> v, Expr b) : names(n), values(v), body(b) {};
+// };
 
 
 struct NumVal{
--- evalExpr.cpp
+++ evalExpr.cpp
@@ -1,58 +1,35 @@
 #include <iostream>
 #include <unordered_map>
-// #include <variant>
-#include <any>
 #include "Expr.hpp"
-
+#include "../Cool-Libs/mpark/patterns.hpp"
 
 typedef std::unordered_map<std::string, NumVal> Environment;
 
-NumVal evalExpr(Expr *e, Environment env){
+class Num{
+public:
+    double value;
+    // Num(double v) : value(v) {};
+    Num operator=(double v){
+        value = v;
+        return *this;
+    }
+};
 
-    if(auto *num = dynamic_cast<Num*>(e)){
-        return NumVal(num->value);
-    }
-    else if(auto *var = dynamic_cast<Var*>(e)){
-        return env[var->name];
-    }
-    else if(auto *add = dynamic_cast<Plus*>(e)){
-        auto l = evalExpr(add->lhs, env);
-        auto r = evalExpr(add->rhs, env);
 
-        return NumVal(l.value + r.value);
-    }
-    else if(auto *sub = dynamic_cast<Minus*>(e)){
-        auto l = evalExpr(sub->lhs, env);
-        auto r = evalExpr(sub->rhs, env);
+// template <typename T>
+NumVal evalExpr(Num e, Environment env){
+    using namespace mpark::patterns;
+    IDENTIFIERS(x);
 
-        return NumVal(l.value - r.value);
-    }
-    else if(auto *div = dynamic_cast<Div*>(e)){
-        auto l = evalExpr(div->lhs, env);
-        auto r = evalExpr(div->rhs, env);
+    return match(e)(
+        pattern(ds(x)) = [](auto &&x){
+            std::cout << "X: " << x << std::endl;
+            return NumVal(10);
+         },
+        pattern(_) = [](){ return NumVal(0); }
+    );
+    
 
-        return NumVal(l.value / r.value);
-    }
-    else if(auto *mul = dynamic_cast<Mult*>(e)){
-        auto l = evalExpr(mul->lhs, env);
-        auto r = evalExpr(mul->rhs, env);
-
-        return NumVal(l.value * r.value);
-    }
-    else if(auto let = dynamic_cast<Let*>(e)){
-        env[let->name] = evalExpr(let->value, env);
-        return evalExpr(let->body, env);
-    }
-    else if(auto multLet = dynamic_cast<MultiLet*>(e)){
-        Environment newEnv = multLet->names.zip(multLet->values).fold<Environment>(env)(
-            [](Environment env, std::pair<std::string, Expr*> nameValue){
-                env[nameValue.first] = evalExpr(nameValue.second, env);
-                return env;
-            }
-        );
-
-        return evalExpr(multLet->body, newEnv);
-    }
 
 
     throw std::runtime_error("Unknown type");
@@ -61,14 +38,10 @@ NumVal evalExpr(Expr *e, Environment env){
 
 
 int main(){
-    // ALT BRANCH
-    
-    // write a test for MultiLet
-
-    Expr *e = new MultiLet(Bector<std::string>("x", "y", "z"), Bector<Expr*>(new Plus(new Num(5), new Num(3)), new Mult(new Num(7), new Num(3)), new Num(10)), new Plus(new Var("x"), new Var("y")));
-
-
+    // Expr e = MultiLet(Bector<std::string>("x", "y", "z"), Bector<Expr>(Plus(Num(5), Num(3)), Mult(Num(7), Num(3)), Num(10)), Plus(Var("x"), Var("y")));
+    auto e = Num(10);
     Environment env;
+
 
     std::cout << evalExpr(e, env) << std::endl;
 
